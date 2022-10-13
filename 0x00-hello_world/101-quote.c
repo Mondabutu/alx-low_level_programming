91db340bfe1e0eaed8bc43a253bb82017437566d
Mondabutu <mondabutu@gmail.com>
2022-10-13 11:14:49 -0700

    You have to be a genius to understand the UNIX simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - write to the stdout
+ * main - write to stdout
+ *  
+ * Return: Always 0
  *
- * Return: 1 if (success)
  */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1)
+	return (1);
+
 }
