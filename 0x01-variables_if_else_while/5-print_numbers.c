d72a6068aaef5d864333146de9d1a2ca08675eb0
Mondabutu <mondabutu@gmail.com>
2022-10-14 20:56:38 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio,h>
+/**
+ * main - print number
+ *
+ * Return: 0 if (success)
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+		printf("%d", numb);
+	printf("\n");
+	return (0);
+}
