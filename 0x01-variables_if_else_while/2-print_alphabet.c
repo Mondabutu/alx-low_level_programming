a10fc3426b85b2c46034f877d86c5b87065df04a
Mondabutu <mondabutu@gmail.com>
2022-10-14 19:10:52 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	char charac;
-	
-	for (charac = 'a'; charac <= 'z'; charac++)	
+
+	for (charac = 'a'; charac <= 'z'; charac++)
 		putchar(charac);
 	putchar ('\n');
 	return (0);
