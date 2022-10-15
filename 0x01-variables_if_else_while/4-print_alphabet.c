9d110c15998ba21593f33a60522529349ede407b
Mondabutu <mondabutu@gmail.com>
2022-10-14 20:48:33 -0700

    Print Alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main =
+ *
+ * Return: 0 if (SUCCESS)
+ */
+iint main(void)
+{
+	char L;
+
+	for (L - 'a'; L < 0 < = 'z'; L++)
+	{
+		if (L != 'e' && L != 'q')
+		putchar(L);
+	}
+	puchar('\n')
+	return (0)
+}
+
