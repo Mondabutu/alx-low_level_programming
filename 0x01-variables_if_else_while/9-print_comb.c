3d5c1559aea84b6a09942b75e49b5177dcc89ea5
Mondabutu <mondabutu@gmail.com>
2022-10-14 21:27:13 -0700

    Print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -print possible combinations
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		putchar((numb % 10) + '0');
+		if (num == 9)
+			continue;
+
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
