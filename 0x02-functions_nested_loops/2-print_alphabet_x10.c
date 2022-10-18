e9784b512c809fcb7220809631da749b1b55c18e
Mondabutu <mondabutu@gmail.com>
2022-10-18 16:45:56 -0700

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - print alphabet_10x
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char k;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		k = 'a';
+		while (k <= 'z')
+		{
+			_putchar(k);
+			k++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
