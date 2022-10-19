cffeba5730820d404b9291bc8ce8aad1a98b26e2
Mondabutu <mondabutu@gmail.com>
2022-10-19 06:54:23 -0700

    Print Fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of the even terms
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{ 
+	int m;
+	unsigned long int n, p, nxt, sum;
+	n = 1;
+	p = 2;
+	sum = 0;
+	for (m = 1; m <= 33; ++i)
+	{
+		if (n < 4000000 && (n % 2) == 0)
+		{
+			sum = sum + n;
+		}
+		nxt = n + p;
+		n = p;
+		p = nxt;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
+
+
+
