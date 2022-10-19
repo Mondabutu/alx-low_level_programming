58eb59a80c38ef54e08fe3244343f4c1de4b1511
Mondabutu <mondabutu@gmail.com>
2022-10-19 04:16:38 -0700

    add two integres

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and return the result
+ * @a: int to be added to to d
+ * @d: int to be added to a
+ * Return: sim of a and d
+ */
+int add (int a, int d)
+{
+	int b;
+
+	b = a + d;
+
+	return (b);
+}
