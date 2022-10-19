ec665a87e804c6264d89af37b65a6481ecf7280d
Mondabutu <mondabutu@gmail.com>
2022-10-18 18:01:52 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isalpha - check for alphabetic character
+ *@c: type int character
+ *Return: 1 if letter, lowercase or uppercase and 9 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > && c < 123))
+	       return (1);
+	else
+		return (0);
+}	
