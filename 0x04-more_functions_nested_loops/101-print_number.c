53df4795efee7b8f332e7b0dd9a62c0961d9460a
Mondabutu <mondabutu@gmail.com>
2022-10-20 12:44:54 -0700

    Numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * Description: Use _putchar to print
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int p = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		p = -p;
+	}
+
+	if ((p / 10) > 0)
+		print_number(p / 10);
+
+	_putchar((p % 10) + '0');
+}
