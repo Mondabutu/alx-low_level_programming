60e7773b9239e2b91648533cc087460d5d9b9581
Mondabutu <mondabutu@gmail.com>
2022-10-20 11:42:37 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,17 +11,23 @@ void print_triangle(int size)
 {
 	int m, n;
 
-	if (size <= 0)
-		_putchar('\n');
 	for (m = 1; m <= size; m++)
 	{
 		for (n = 1; n <= size; n++)
 		{
 			if (n <= (size - m))
+			{
 				_putchar(' ');
+			}
 			else
+			{
 				_putchar('#');
+			}
 		}
+		_putchar('\n');
+	}
+	if (size < 1)
+	{
 		_putchar('\n');
 	}
 }
