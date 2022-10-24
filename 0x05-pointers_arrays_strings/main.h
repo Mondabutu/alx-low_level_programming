bafcb555979af9bca484d107dd6aacea05609b86
Mondabutu <mondabutu@gmail.com>
2022-10-24 11:37:35 -0700

    main header

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
+#ifndef MAIiN_H
 #define MAIN_H
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
-int _strlen(char *s)
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -11,4 +11,4 @@ void puts(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#endif
+#endifi
