1d2b3900776b61bd0b1aedf5c09b271b42d48f13
Mondabutu <mondabutu@gmail.com>
2022-10-24 14:50:07 -0700

    Find character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts - function that prints every other character of a string, starting with the first
+ *  charater, followed by a new line
+ *  @str: string
+ *  Return character
+ */
+
+void puts2(char *str)
+{
+	int m;
+	int n;
+
+	for (m = 0; str[m] != '\0'; m++)
+	{
+		:
+	}
+	for (n = 0; n < m; n = n + 2)
+	{
+		_putchar(str[n])
+	}
+	_putcar('\n');
+}
+
