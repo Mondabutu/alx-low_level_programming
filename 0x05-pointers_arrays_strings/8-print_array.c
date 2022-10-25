231629f5c76bd28cae34e60bb1b5609f2a6e08f2
Mondabutu <mondabutu@gmail.com>
2022-10-24 17:17:43 -0700

    Print Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 
 	for (m = 0; m < n; m++)
 	{
-		printf("%d", a[m]);
-		if (m < n - 1)
+		printf("%d", *(a + m));
+		if (m != n - 1)
 		{
 			printf(",");
 		}
 	}
-	printf('\n');
+	printf("\n");
 }
 
