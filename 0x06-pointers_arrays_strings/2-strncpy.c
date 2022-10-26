f8395e727a771e18a1002decf64142a272f4574d
Mondabutu <mondabutu@gmail.com>
2022-10-26 16:49:27 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpy - copies string from src to dest
+ * @dest: where the string is stored
+ * @src: The source string
+ * @n: Maximum number of bytes to be copied
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+	int m = 0, k = 0;
+
+
+
+	while (src[m++])
+
+		k++;
+
+	for (m = 0; src[m] && m < n; m++)
+
+		dest[m] = src[m];
+
+	for (m = k; m < n; m++)
+
+		dest[m] = '\0';
+
+
+		return (dest);
+
+}
