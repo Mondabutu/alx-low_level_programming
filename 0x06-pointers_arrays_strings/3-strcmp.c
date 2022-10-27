c15478d4686cb4f16615a67a1cd4823ffcb1978d
Mondabutu <mondabutu@gmail.com>
2022-10-26 18:14:15 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - comparing two strings
+ * @s1: First input String
+ * @s2: Second input string
+ *
+ * Return: b if different and 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int m = 0;
+	int n = 0;
+
+	while (s1[m] != '\0' && n == 0)
+	{
+		b = s1[m] - s2[m];
+		m++;
+	}
+	return (n);
+}
