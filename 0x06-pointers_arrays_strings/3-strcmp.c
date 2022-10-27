1a70b4721a510380fa8101e5894eda0351d5fa0b
Mondabutu <mondabutu@gmail.com>
2022-10-26 18:26:58 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,15 +6,14 @@
  *
  * Return: b if different and 0 if equal
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	int m = 0;
-	int n = 0;
 
-	while (s1[m] != '\0' && n == 0)
+	while ((s1[m] == s2[m]) && s1[m] != '\0')
 	{
-		b = s1[m] - s2[m];
 		m++;
 	}
-	return (n);
+	return (s1[m] - s2[m]);
 }
