39cb9ec010f29cdc3823ab6b9d4b224f42713278
Mondabutu <mondabutu@gmail.com>
2022-10-27 15:45:18 -0700

    Rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,30 +3,34 @@
 /**
  * rot13 - encode a string using rot13
  * @s: the string to encode
- *
+ * Description: use rot13 rules to change all alpha characters
  * Return: the encode string
  */
 char *rot13(char *s)
 {
-	char i, j;
+	int i = 0;
+	int j;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rt[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
-	i = 0;
-	while (s[i] != '\0')
+	char a[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'. 'O', 'P', 'Q',
+		'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',  'u', 'v', 'w', 'x', 'y', 'z'};
+	char rt[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C',
+		'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+
+	while (s[i] != 0)
 	{
-		j = 0;
-		while (a[j] != '\0')
+		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == a[j]);
+			if (s[i] == a[j])
 			{
 				s[i] = rt[j];
 				break;
 			}
-			j++;
+
 		}
 		i++
 	}
 	return (s)
-}					
+}
