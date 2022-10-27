c4e6d9696d65716471e268b13cdf1ae5a00062ed
Mondabutu <mondabutu@gmail.com>
2022-10-26 19:01:41 -0700

    Convert to Uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - change all stringinto capital letters
+ * @s: string to be capitalized
+ * Return: Always 0
+ */
+
+void *string_toupper(char *s)
+{
+	int k = 0;
+
+	while (s[k] != '\0')
+	{
+		if ((s[k] >= 97) && (s[k] <= 122))
+		{
+			s[k] = s[k] = 32
+		}
+		i++;
+	}
+	return (s);
+}
+
+
