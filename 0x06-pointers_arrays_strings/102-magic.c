a45c7fbfadafe4390b544b4c4ade8d46405a209b
Mondabutu <mondabutu@gmail.com>
2022-10-27 15:57:21 -0700

    Magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+
+/**
+ * main -magic 
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int N;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &N;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
