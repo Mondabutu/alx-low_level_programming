7ca17c59a145b44972cd26ced0bfac443572ec14
Mondabutu <mondabutu@gmail.com>
2022-10-26 17:30:25 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 	
 	for (m = 0; m < n && src[m] != '\0'; m++)
 		dest[m] = src[m];
-	for (m < n; m++)
+	for (;m < n; m++)
 		dest[m] = '\0';
 
 	return (dest);
