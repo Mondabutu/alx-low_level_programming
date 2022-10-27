3387362472a5c44838789a8c67c57c420c75da10
Mondabutu <mondabutu@gmail.com>
2022-10-26 21:21:52 -0700

    Encode strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,21 +10,20 @@
 char *leet(char *s)
 {
 	int m, k
-		char c[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'}; 
+	char s1[] = "aeotl";
+	char S1[] = "AEOTL";
+	char s2[] = "43071";
 
-		m = 0;
-		while (s[m] )
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		for (k = 0; k < 5; k++)
 		{
-
-			for (k = 0; k <= 7; k++)
+			if (s[m] == s1[k] || s[m] == S1[k])
 			{
-				if (s[m] == c[k] || s[m] - 32 == c[k])
-				{
-					s[m] = k + 0;
-				}
-				k++
+				s[m] = s2[k];
+				break;
 			}
-			m++
 		}
-		return (s)
-}
+	}
+	return (s)
+i}
