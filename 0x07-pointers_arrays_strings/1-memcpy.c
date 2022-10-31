3920b0ec78982df8697edfef2c58522c5fa0f6fd
Mondabutu <mondabutu@gmail.com>
2022-10-31 14:42:55 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - functions that copies memory are
+ * @dest: destination of character
+ * @src: source of character
+ * @n: unsigned integer
+ * Return: memory content
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k
+
+	for (k = 0; k < n; k++)
+	{
+		dest[k] = src[k];
+	}
+	return (dest);
+}
