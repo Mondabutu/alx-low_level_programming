26efb2fe0f98ddcfa17804ad0a9af2f9ad97fc47
Mondabutu <mondabutu@gmail.com>
2022-11-02 11:00:33 -0700

    Print String lenght

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - return the length of string
+ * _strlen_recursion - returns the length of string
  * @s: The string to return length
- * Retun : String lenght
+ * Return: String lenght
  */
 int _strlen_recursion(char *s)
 {
-	if (*s !='\0')
+	if (*s != '\0')
 	return (1 + _strlen_recursion(s + 1));
 	return (0);
 }
