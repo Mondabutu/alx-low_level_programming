b31574969932d3e63272c413c7c8e72078b991d6
Mondabutu <mondabutu@gmail.com>
2022-11-04 06:30:06 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the program Name
+ * @argc: Counts the arguments
+ * @argv: The arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
