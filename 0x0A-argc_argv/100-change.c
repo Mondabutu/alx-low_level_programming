06b3419d9f50fcbbc51660d5bcae054adf2d953a
Mondabutu <mondabutu@gmail.com>
2022-11-04 07:27:51 -0700

    prints the minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -prints the minimum number of coins
+ * @argc: Lenght of the argv
+ * @argv: Number of the argument
+ * Return: Alayws 0
+ */
+int main(int argc, char *argv[])
+{
+	int pos, sum, change, extra;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	pos = extra = change = sum = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum = atoi(argv[1]);
+	if (sum <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (coins[pos] != '\0')
+	{
+		extra = (sum / coins[pos]);
+		change += extra;
+		sum -= coins[pos] * extra;
+	}
+	pos++;
+}
+print("%d\n", change);
+
+return (0);
+}
