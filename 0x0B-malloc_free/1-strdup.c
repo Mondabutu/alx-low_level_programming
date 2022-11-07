a494525d7d28b241dd4162c1fdbfe2ef44a0a6d0
Mondabutu <mondabutu@gmail.com>
2022-11-07 10:10:47 -0800

    newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - count the number of array
+ * @s: elements in the array
+ * Return: 1
+ */
+int _strlen(char *s)
+{
+	unsigned int k;
+
+	k = 0;
+	while (s[k] != '\0')
+	{
+		k++;
+	}
+	return (k);
+}
+/**
+ * _strcpy - This copies the arrays
+ * @src: Elements of the array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int k = 0;
+
+	while (src[k] != '\0')
+	{
+		dest[k] = src[k];
+		k++;
+	}
+	dest[k] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strdup - string duplicates and prints strings array
+ * @str: The elements of array
+ * Return: the pointer
+ */
+char *_strdup(char *str)
+{
+	char *d;
+	unsigned int m;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	m = _strlen(str) + 1;
+	d = (char *) malloc(size * sizeof(char));
+	if (d == 0)
+	{
+		return (NULL);
+	}
+	_strcpy(dst, str);
+	return (dst);
+}
