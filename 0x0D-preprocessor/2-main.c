abb356d50f101bae353cb204bc3f3b8bdd9830c6
Mondabutu <mondabutu@gmail.com>
2022-11-15 04:37:32 -0800

    Print File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the name of the file it was compiled from, followed by a new line
+ * Description: this program prints the name of the file it was compiled from
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n",_FILE_)
+		return (0);
+}
