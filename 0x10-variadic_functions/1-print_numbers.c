f91c7defc4b3d3974c2b508c2c3b0783ce8c4bc4
Mondabutu <mondabutu@gmail.com>
2022-11-17 06:06:08 -0800

    Print Numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - printsthe  numbers
+ *  @separator: pointer parameters
+ *  @n:parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int k;
+
+	va_start(valist, n);
+	for (k = 0; k < n; k++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(valist);
+}
