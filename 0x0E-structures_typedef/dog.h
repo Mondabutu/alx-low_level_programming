79d537305db9f15657c299de30990a517acdb69a
Mondabutu <mondabutu@gmail.com>
2023-04-21 21:13:48 +0100

    Structures Typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - a dog's basic info
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Who is the owner of the dog
- *
- * Description: No description for this struct
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
+
 struct dog
 {
 	char *name;
@@ -17,15 +16,14 @@ struct dog
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-#endif
+#endif /* _DOG_H_ */
