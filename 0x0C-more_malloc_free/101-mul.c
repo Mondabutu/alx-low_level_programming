a18c60f5aa79ab68a37f004878b8028702c18a85
Mondabutu <mondabutu@gmail.com>
2023-04-30 05:04:23 -0700

    utiplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,63 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * _isdigit - Define if a string is a number
- * @argv: The string Pointer
- * Return: Always 0
- */
-int _isdigit(char *argv)
-{
-	int k;
-
-	for (k = 0; argv[k]; k++)
-		if (argv[k] < 48 || argv[k] > 57)
-			return (1);
-	return (0);
-}
-/**
- * _atoi - converts a string of ascii digits to the values they represent
- * @s: pointer to the source string
- * Return: the digits value
- */
-int _atoi(char *s)
-{
-	int k, r;
-
-	k = r = 0;
-	while (s[k])
-	{
-		if (s[k] >= '0' && s[k] <= '9')
-		{
-			r *= 10;
-			r += (s[k] - '0');
-		}
-		k++;
-	}
-	return (r);
-}
-/**
- * main - The main function
- * @argc: The argument counter
- * @argv: 2D array of arguments
- * Return: return 0 on success, 98 on failure
- */
+* main - multiply 2 numbers (num1, num2) as arguments
+* @argc: number of arguments
+* @argv: arguments
+* Return: 0 success
+*/
 int main(int argc, char *argv[])
 {
-	int k;
-
+unsigned long mul;
+int num1, num2;
 	if (argc != 3)
+	{printf("Error\n");
+	exit(98); }
+	for (num1 = 1; num1 < argc; num1++)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (k = 1; k < argc; k++)
-	{
-		if (_isdigit(argv[k]))
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
-			printf("Error\n");
-			exit(98);
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+			{  printf("Error\n");
+			exit(98); }
 		}
+
 	}
-	return (0);
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
