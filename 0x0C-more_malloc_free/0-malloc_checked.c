7799641d615605571e7b214db96e8e9f6d5ced3b
Mondabutu <mondabutu@gmail.com>
2023-04-30 05:00:46 -0700

    Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,19 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-#include <limits.h>
+#include <stdlib.h>
 
 /**
- * malloc_checked - function that allocates memory using malloc
- * @b: The integet variable which hold the memory size
+ * malloc_checked - allocates memory using malloc
+ * @b: memory to allocate
  *
- * Return: Alwys return 0 (Success)
+ * Return: pointer to allocated memory or normal process termination if error
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 
 	p = malloc(b);
-
 	if (p == NULL)
-	       exit(98);
+		exit(98);
 	return (p);
 }
