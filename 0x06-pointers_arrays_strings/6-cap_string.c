4e581137be763b70170f39686ad6e77e073f5a70
Mondabutu <mondabutu@gmail.com>
2023-04-30 04:19:05 -0700

    Cap String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,44 @@
-#include "main.h"
+int check_seperators(char c);
 
 /**
- * cap_string - capitalized all strings words
- * @s: the string to change the first letter of the word into uppercase
- *
- * Return: uppercase letters
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
+ * Return: pointer to s
  */
 
 char *cap_string(char *s)
 {
-	int m = 0, n = 0;
-	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+	int i = 0;
 
-	while (s[m] != '\0')
+	while (s[i])
 	{
-		if (m == 0 && s[m] >= 97 && s[m] <= 122)
-		{
-			s[m] = s[m] - 32;
-		}
-		while (c[n] != '\0')
-		{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
 
-			if (c[n] == s[m] && (s[m + 1] >= 97 && s[m + 1] <= 122))
-			{
-			s[m + 1] = s[m + 1] - 32;
-			}
-			n++;
-		}
-		m++;
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
 	}
+
 	return (s);
+}
+
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }
